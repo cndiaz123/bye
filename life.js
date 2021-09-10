d47fe79a9280a96ea49f4aaf7f2a54c5a2a25a75
cndiaz123 <cndiaz123@gmail.com>
2021-09-10 14:32:49 -0700

    Update life.js

--- life.js
+++ life.js
@@ -22,7 +22,7 @@
                 countdown = document.getElementById("countdown"),
                 content = document.getElementById("content");
   
-            headline.innerText = "Goodbye!";
+            headline.innerText = "Goodbye, you will be missed..Goodluck!!";
             countdown.style.display = "none";
             content.style.display = "block";
   
